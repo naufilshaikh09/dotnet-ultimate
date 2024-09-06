83292bff9b39fc39a59768e655b43321246b1994
Naufil Shaikh <snaufil09@gmail.com>
2024-09-06 10:13:06 +0530

    Added serilog support

--- Program.cs
+++ Program.cs
@@ -1,25 +1,62 @@
-var builder = WebApplication.CreateBuilder(args);
+using dotnet_ultimate.Services;
+using Serilog;
 
-// Add services to the container.
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    Log.Information("Starting up");
+    
+    var builder = WebApplication.CreateBuilder(args);
+    
+    // Approach 1: Configuration via appSettings.json
+    builder.Host.UseSerilog((context, loggerConfiguration) =>
+    {
+        loggerConfiguration.WriteTo.Console();
+        loggerConfiguration.ReadFrom.Configuration(context.Configuration);
+    });
+
+    // Approach 2: Configuration via Fluent API 
+    // builder.Host.UseSerilog((context, logger) =>
+    // {
+    //     logger.MinimumLevel.Information();
+    //     logger.WriteTo.Console();
+    // });
+
+    // Add services to the container.
+    builder.Services.AddControllers();
+
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    builder.Services.AddTransient<IDummyService, DummyService>();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+    
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.MapControllers();
+    
+    // Minimal api
+    app.MapGet("/", (IDummyService svc) => svc.DoSomething());
+    
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
---
+++ Services/IDummyService.cs
@@ -0,0 +1,6 @@
+namespace dotnet_ultimate.Services;
+
+public interface IDummyService
+{
+    void DoSomething();
+}
